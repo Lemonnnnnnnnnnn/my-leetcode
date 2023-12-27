fba1050499492bc4fb32f11838b51db46d0604bb
soul <1041707577@qq.com>
2023-12-27 09:57:09 +0800

    feat: update length of last word

--- src/58.length-of-last-word.ts
+++ src/58.length-of-last-word.ts
@@ -5,21 +5,46 @@
  */
 
 // @lc code=start
+// 从前往后
+// function lengthOfLastWord(s: string): number {
+//     let res = 0;
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === ' ') {
+//             if (s[i + 1] && s[i + 1] !== " ") {
+//                 res = 0
+//             }
+//         } else {
+//             res++
+//         }
+//     }
+//     return res
+// };
+
+// 从后往前
 function lengthOfLastWord(s: string): number {
     let res = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === ' ') {
-            if (s[i + 1] && s[i + 1] !== " ") {
-                res = 0
+    let findWord = false;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (!findWord) {
+            if (s[i] === " ") {
+                continue
+            } else {
+                findWord = true;
+                res++;
             }
         } else {
-            res++
+            if (s[i] === " ") {
+                break
+            } else {
+                res++;
+            }
         }
     }
     return res
-};
+}
 // @lc code=end
 
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
+// console.log(lengthOfLastWord("   fly me   to   the moon  "));
+console.log(lengthOfLastWord("Hello World"));
 
